ef74431c420310a9a84f0e80878df1477f7a0a9f
Jerry Davidson <willdjerry@gmail.com>
2019-08-29 20:00:37 -0400

    build files added for surge

---
+++ build/precache-manifest.0b60e69eb68f5ddc07bc1c1e9025c223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fb182755494fb0179336c5c95ae77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89680a0f5661ad85e3ae",
+    "url": "/static/css/main.730a25a4.chunk.css"
+  },
+  {
+    "revision": "9981ade106a8c16e0eb0",
+    "url": "/static/js/2.02fcefa9.chunk.js"
+  },
+  {
+    "revision": "89680a0f5661ad85e3ae",
+    "url": "/static/js/main.30c07e7f.chunk.js"
+  },
+  {
+    "revision": "2899c75f0a5eecd0b73d",
+    "url": "/static/js/runtime~main.84899951.js"
+  }
+]);
