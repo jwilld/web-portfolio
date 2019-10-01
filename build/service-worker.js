fc3edd9a46ebfcc95b15cda24d546f673d2f97de
Jerry Davidson <willdjerry@gmail.com>
2019-10-01 19:53:00 -0400

    google chrome's phone emulator doesn't accurately presesnt the portfolio images. Modal image width changed to 40% to test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1aedd654b7c7875915fbda573e543a11.js"
+  "/precache-manifest.5c6afca2442d3835a986dd019f3fe379.js"
 );
 
 self.addEventListener('message', (event) => {
